8a448ce93237a05c4741c47b49cd9ca142f26e98
BIG DON <BIG DON@MSI>
2024-11-14 09:15:14 -0600

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace ASSIGNMENT2_3_1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string filePath = @"C:\Users\BIGDON\source\repos";
+
+            List<string> lines = File.ReadAllLines(filePath).ToList();  
+
+            foreach (string line in lines)
+            {
+                Console.WriteLine(line);
+            }
+            File.WriteAllLines(filePath, lines);
+
+            Console.ReadLine();
+
+
+        }
+    }
+}
